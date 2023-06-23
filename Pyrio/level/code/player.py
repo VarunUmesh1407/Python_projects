69b570ad53cf78b20a3c36eebb8ea0941ad6eb5f
VarunUmesh1407 <Varun.Umesh1407@gmail.com>
2023-06-23 13:08:19 +0200

    PYR-1 player movement

---
+++ Pyrio/level/code/player.py
@@ -0,0 +1,46 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((32, 64))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(topleft=pos)
+
+        # player movement
+        self.direction = pygame.math.Vector2(0, 0)
+        self.speed = 6
+        self.gravity = 0.8
+        self.jump_speed = -16
+
+    def import_charcter_assets(self):
+        character_path = '../graphics/character/'
+        self.animations = {'idle': [], 'run': [], 'jump':[], 'fall': []}
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+        if keys[pygame.K_SPACE]:
+            self.jump()
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
+
+    def update(self):
+        self.get_input()
---
+++ Pyrio/level/code/support.py
@@ -0,0 +1,4 @@
+from os import walk
+
+def import_folder(path):
+    for 1,2,3 in walk(path)
